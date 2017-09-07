73b76e956b4894aa48fedbaacdd38c7c688f2e87
jontte75 <joni.korhonen@gmail.com>
2017-09-07 21:48:09 +0300

    I2C CPP POC
    Modified so that i2c communication is done in one thread (forever-loop)
    An other thread will handle sending to thingSpeak, when ever i2c-thread
    inform that it has data...classic producer consumer thing...

--- omega/cpp/main.cpp
+++ omega/cpp/main.cpp
@@ -20,43 +20,96 @@
 #include <cstddef>
 #include <iostream>
 #include <cmath>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
 #include "onion-i2c.h"
 #include "i2cdata.h"
 #include "i2ccom.h"
 
-int main(void)
-{
+std::mutex mtex;
+std::condition_variable cv;
+std::queue<I2cData> dataQueue;
+
+bool success=false;
+
+void i2cComThr(int retries) {
     I2cData set1;
     I2cData set2;
     I2cCom conn;
     //connect to arduino (check i2c with simple hello)
     conn.connect();
-
-    int retries=5;
-    while(retries){
-        conn.storeDataFromI2c(I2C_CMD_GET_DATA_SET1);
-        if(!set1.setData(conn.getData(),25))
+    
+    while(1){
         {
-            std::cout << "Ohno invalid data from i2c" << std::endl;
-            retries--;
-            sleep(10);
-            continue;
+            std::lock_guard<std::mutex> lk(mtex);
+            while(retries){
+                conn.storeDataFromI2c(I2C_CMD_GET_DATA_SET1);
+                if(!set1.setData(conn.getData(),25))
+                {
+                    std::cout << "Ohno invalid data from i2c" << std::endl;
+                    retries--;
+                    success = false;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+                    continue;
+                }
+                // std::cout << "Data recieved:" << set1 <<std::endl;
+                dataQueue.push(set1);
+                success = true;
+                break;
+            }
+            retries=5;
+            while(retries){
+                conn.storeDataFromI2c(I2C_CMD_GET_DATA_SET2);
+                if(!set2.setData(conn.getData(),25))
+                {
+                    std::cout << "Ohno invalid data from i2c" << std::endl;
+                    retries--;
+                    success = false;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+                    continue;
+                }
+                success = true;
+                // std::cout << "Data recieved:" << set2 <<std::endl;
+                dataQueue.push(set2);
+                break;
+            }
         }
-        std::cout << "Data recieved:" << set1 <<std::endl;
-        break;
+        if (success){
+            cv.notify_all();
+        }else{
+            while(!dataQueue.empty()){
+                dataQueue.pop();    
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(300));
     }
-    retries=5;
-    while(retries){
-        conn.storeDataFromI2c(I2C_CMD_GET_DATA_SET2);
-        if(!set2.setData(conn.getData(),25))
-        {
-            std::cout << "Ohno invalid data from i2c" << std::endl;
-            retries--;
-            sleep(10);
-            continue;
+}
+
+void thingSpeakThr() {
+	while (true) {
+        //wait requires unique_lock
+        std::unique_lock<std::mutex> lk(mtex);
+        cv.wait(lk, []{return success || !dataQueue.empty(); });
+		while (!dataQueue.empty()) {
+                std::cout<<"--------------------"<<std::endl;
+                std::cout<<dataQueue.front();
+                std::cout<<std::endl;
+                std::cout<<"--------------------"<<std::endl;
+                dataQueue.pop();
         }
-        std::cout << "Data recieved:" << set2 <<std::endl;
-        break;
-    }    
+	}
+}
+
+int main(void)
+{
+	std::thread t1(&i2cComThr, 5);
+	std::thread t2(&thingSpeakThr);
+
+    t1.join();
+    t2.join();
+ 
     return 0;
 }
